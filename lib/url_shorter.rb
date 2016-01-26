f288c8164e0e8b300fd77f8ec0d2b07a8e6ada16
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-26 16:58:34 +0900

    ライブラリ実装

--- lib/url_shorter.rb
+++ lib/url_shorter.rb
@@ -1,5 +1,37 @@
 require "url_shorter/version"
 
 module UrlShorter
-  # Your code goes here...
+  require 'net/http'
+  require 'uri'
+  require 'json'
+
+  class UrlShorter
+
+    attr_reader :short_url
+
+    def initialize(api_key, long_url)
+      @api_key = api_key
+      @long_url = long_url
+      _create_short_url
+    end
+
+    def _create_short_url
+      @short_url = _request_api
+    end
+
+    def _request_api
+      uri = URI.parse("https://www.googleapis.com/urlshortener/v1/url?key=#{@api_key}")
+
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+
+      req = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' => 'application/json'})
+      req.body = {longUrl: @long_url}.to_json
+
+      res = https.request(req)
+      result = JSON.parse(res.body)
+      result['id']
+    end
+  end
+
 end
