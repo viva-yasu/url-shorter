178a20a6926b29ff5211e8d1baef9cbdf9bf3423
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-26 17:29:26 +0900

    rspecの作成 #3

--- spec/url_shorter_spec.rb
+++ spec/url_shorter_spec.rb
@@ -5,7 +5,8 @@ describe UrlShorter do
     expect(UrlShorter::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'generate short URL' do
+    url_shorter = UrlShorter::UrlShorter.new('AIzaSyDk8JonB2nEr8nf4C0W4ztDlJDmlDQ-z0U', 'https://github.com/viva-yasu/url-shrinker/')
+    expect(url_shorter.short_url).to eq('https://goo.gl/o43GnB')
   end
 end
